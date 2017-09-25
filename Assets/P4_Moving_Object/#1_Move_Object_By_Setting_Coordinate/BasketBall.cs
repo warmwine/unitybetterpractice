1691837d90b3fa187b7bf1bb7e81ea7b04c80b36
温酒 <zj_aznable@aliyun.com>
2017-09-25 22:18:14 +0800

    添加了简单的移动物体例子

---
+++ Assets/P4_Moving_Object/#1_Move_Object_By_Setting_Coordinate/BasketBall.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MovingObject
+{
+    public class BasketBall : MonoBehaviour
+    {
+        private float speed = 0.0f;
+        private float acceleration = 0.01f;
+        private Vector3 currentPos = new Vector3 { };
+        // Use this for initialization
+        void Start()
+        {
+            this.currentPos = this.transform.position;
+            this.currentPos.x = -10;
+            this.transform.position = this.currentPos;
+            this.speed = 0.0f;
+            this.acceleration = 0.01f;
+        }
+
+        void FixedUpdate()
+        {
+            this.currentPos.x = this.currentPos.x + this.speed;
+            this.transform.position = this.currentPos;
+            this.speed = this.speed + this.acceleration;
+            if (this.speed > 0.4f || this.speed < -0.4f)
+            {
+                this.acceleration = -this.acceleration;
+            }
+        }
+    }
+}
---
+++ Assets/P4_Moving_Object/#2_Gravity_By_Code/CodeGravity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MovingObject
+{
+    public class CodeGravity : MonoBehaviour
+    {
+        private float speed = 0.0f;
+        private float acceleration = -0.0098f;
+        private Vector3 currentPos = new Vector3 { };
+        // Use this for initialization
+        void Start()
+        {
+            this.currentPos = this.transform.position;
+            this.speed = 0.0f;
+            this.acceleration = -0.0098f;
+        }
+
+        void FixedUpdate()
+        {
+            this.speed = this.speed + this.acceleration;
+            this.currentPos.y = this.currentPos.y + this.speed;
+            this.transform.position = this.currentPos;
+            if (this.currentPos.y < -4)
+            {
+                this.speed = 0.4f;
+            }
+        }
+    }
+}
