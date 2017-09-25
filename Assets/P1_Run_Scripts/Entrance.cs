b62a8fb4f1c763dfc2871da33a1917b71f5f2042
温酒 <zjwjxdm@gmail.com>
2017-09-25 15:51:36 +0800

    修正一下ignore文件，添加最简单的demo

---
+++ Assets/P1_Run_Scripts/Entrance.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entrance : MonoBehaviour
+{
+    protected int updatedTimes = 0;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        Debug.Log("Application start, entrance awaked");
+    }
+
+    /// <summary>
+	/// Start is called on the frame when a script is enabled just before
+	/// any of the Update methods is called the first time.
+	/// </summary>
+	void Start()
+    {
+        Debug.Log("Application start, entrance started");
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        this.updatedTimes++;
+        Debug.Log("Application start, entrance updated " + this.updatedTimes + " times");
+    }
+}
