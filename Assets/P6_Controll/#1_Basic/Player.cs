a24fc95c84cd13152432f64d977507d3d6e4c706
温酒 <zj_aznable@aliyun.com>
2017-11-22 21:55:49 +0800

    修复一个因为直接设置position引起部分穿墙的问题

--- Assets/P6_Controll/#1_Basic/Player.cs
+++ Assets/P6_Controll/#1_Basic/Player.cs
@@ -2,120 +2,116 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace G1_20Sec_1
+public class Player : MonoBehaviour
 {
-    public class Player : MonoBehaviour
+    private Mesh mesh = null;
+    private MeshRenderer meshRenderer = null;
+    private Rigidbody2D body = null;
+    public float speed = 1000.0f;
+
+    void CreatePlayer()
     {
-        private Mesh mesh = null;
-        private MeshRenderer meshRenderer = null;
+        this.mesh = gameObject.AddComponent<MeshFilter>().mesh;//网格对象
+        this.meshRenderer = gameObject.AddComponent<MeshRenderer>();//网格渲染器
+        this.meshRenderer.material = new Material(Shader.Find("Sprites/Default"));//材质
 
-        public float speed = 0.2f;
+        Vector3[] vertexes = new Vector3[4];
+        vertexes[0] = new Vector3(0, 0, 0);
+        vertexes[1] = new Vector3(1, 0, 0);
+        vertexes[2] = new Vector3(1, 1, 0);
+        vertexes[3] = new Vector3(0, 1, 0);
+        this.mesh.vertices = vertexes;
 
-        void CreatePlayer()
+        Color32[] colors32 = new Color32[4];
+        colors32[0] = new Color32(255, 0, 0, 255);
+        colors32[1] = new Color32(255, 0, 0, 255);
+        colors32[2] = new Color32(255, 0, 0, 255);
+        colors32[3] = new Color32(255, 0, 0, 255);
+        this.mesh.colors32 = colors32;
+
+        int[] triangles = new int[] { 0, 2, 1, 0, 3, 2 };
+        this.mesh.triangles = triangles;
+
+        Vector3[] normals = new Vector3[4];
+        normals[0] = new Vector3(0, 0, -10);
+        normals[1] = new Vector3(0, 0, -10);
+        normals[2] = new Vector3(0, 0, -10);
+        normals[3] = new Vector3(0, 0, -10);
+        this.mesh.normals = normals;
+    }
+
+    void CreateCollider()
+    {
+        gameObject.AddComponent<BoxCollider2D>().size = new Vector2(1, 1);
+        this.body = gameObject.AddComponent<Rigidbody2D>();
+        this.body.gravityScale = 0.0f;
+    }
+
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        this.CreatePlayer();
+        this.CreateCollider();
+    }
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        float angel = 0.0f;
+        float moving = 1.0f;
+
+        if (Input.GetKey(KeyCode.A))
         {
-            this.mesh = gameObject.AddComponent<MeshFilter>().mesh;//网格对象
-            this.meshRenderer = gameObject.AddComponent<MeshRenderer>();//网格渲染器
-            this.meshRenderer.material = new Material(Shader.Find("Sprites/Default"));//材质
-
-            Vector3[] vertexes = new Vector3[4];
-            vertexes[0] = new Vector3(0, 0, 0);
-            vertexes[1] = new Vector3(1, 0, 0);
-            vertexes[2] = new Vector3(1, 1, 0);
-            vertexes[3] = new Vector3(0, 1, 0);
-            this.mesh.vertices = vertexes;
-
-            Color32[] colors32 = new Color32[4];
-            colors32[0] = new Color32(255, 0, 0, 255);
-            colors32[1] = new Color32(255, 0, 0, 255);
-            colors32[2] = new Color32(255, 0, 0, 255);
-            colors32[3] = new Color32(255, 0, 0, 255);
-            this.mesh.colors32 = colors32;
-
-            int[] triangles = new int[] { 0, 2, 1, 0, 3, 2 };
-            this.mesh.triangles = triangles;
-
-            Vector3[] normals = new Vector3[4];
-            normals[0] = new Vector3(0, 0, -10);
-            normals[1] = new Vector3(0, 0, -10);
-            normals[2] = new Vector3(0, 0, -10);
-            normals[3] = new Vector3(0, 0, -10);
-            this.mesh.normals = normals;
-        }
-
-        void CreateCollider()
-        {
-            gameObject.AddComponent<BoxCollider2D>().size = new Vector2(1, 1);
-            gameObject.AddComponent<Rigidbody2D>().gravityScale = 0.0f;
-        }
-
-
-        /// <summary>
-        /// Start is called on the frame when a script is enabled just before
-        /// any of the Update methods is called the first time.
-        /// </summary>
-        void Start()
-        {
-            this.CreatePlayer();
-            this.CreateCollider();
-        }
-
-        /// <summary>
-        /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
-        /// </summary>
-        void FixedUpdate()
-        {
-            float angel = 0.0f;
-            float moving = 1.0f;
-
-            if (Input.GetKey(KeyCode.A))
+            angel = Mathf.PI;
+            if (Input.GetKey(KeyCode.S))
             {
-                angel = Mathf.PI;
-                if (Input.GetKey(KeyCode.S))
-                {
-                    angel = angel + Mathf.PI * 0.25f;
-                }
-                if (Input.GetKey(KeyCode.W))
-                {
-                    angel = angel - Mathf.PI * 0.25f;
-                }
+                angel = angel + Mathf.PI * 0.25f;
             }
-            else if (Input.GetKey(KeyCode.D))
+            if (Input.GetKey(KeyCode.W))
             {
-                angel = 0.0f;
-                if (Input.GetKey(KeyCode.S))
-                {
-                    angel = Mathf.PI * 1.75f;
-                }
-                if (Input.GetKey(KeyCode.W))
-                {
-                    angel = Mathf.PI * 0.25f;
-                }
+                angel = angel - Mathf.PI * 0.25f;
+            }
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            angel = 0.0f;
+            if (Input.GetKey(KeyCode.S))
+            {
+                angel = Mathf.PI * 1.75f;
+            }
+            if (Input.GetKey(KeyCode.W))
+            {
+                angel = Mathf.PI * 0.25f;
+            }
+        }
+        else
+        {
+            angel = 0.0f;
+            if (Input.GetKey(KeyCode.S))
+            {
+                angel = Mathf.PI * 1.5f;
+            }
+            else if (Input.GetKey(KeyCode.W))
+            {
+                angel = Mathf.PI * 0.5f;
             }
             else
             {
-                angel = 0.0f;
-                if (Input.GetKey(KeyCode.S))
-                {
-                    angel = Mathf.PI * 1.5f;
-                }
-                else if (Input.GetKey(KeyCode.W))
-                {
-                    angel = Mathf.PI * 0.5f;
-                }
-                else
-                {
-                    moving = 0.0f;
-                }
+                moving = 0.0f;
             }
-
-
-            float cx = this.speed * Mathf.Cos(angel) * moving;
-            float cy = this.speed * Mathf.Sin(angel) * moving;
-            Vector3 pos = transform.position;
-            pos.x += cx;
-            pos.y += cy;
-            transform.position = pos;
         }
+
+        float cx = this.speed * Mathf.Cos(angel) * moving;
+        float cy = this.speed * Mathf.Sin(angel) * moving;
+        print(cx);
+        print(cy);
+        this.body.velocity = new Vector2(cx, cy);
     }
 }
 
